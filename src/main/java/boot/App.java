83cd9c47f3dcab33b8c0555884bd3913eea92fca
Artemev Roman <rom1000rom@yandex.ru>
2019-09-15 11:41:15 +0300

    Added a bean data source for testing with the "test" profile.

--- src/main/java/boot/App.java
+++ src/main/java/boot/App.java
@@ -1,13 +1,19 @@
 package boot;
 
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
 
-
+/**Класс представляет собой java-конфигурацию Spring Context а также точку входа
+ * в Spring Boot приложение.
+ @author Артемьев Р.А.
+ @version 15.09.2019 */
 @SpringBootApplication
 public class App
 {
@@ -16,6 +22,7 @@ public class App
         SpringApplication.run(App.class, args);
     }
 
+    /**Метод возвращает бин-источник данных для готового приложения*/
     @Bean("dataSource")
     public DriverManagerDataSource getDriverManager()
     {
@@ -27,13 +34,27 @@ public class App
         return driverManager;
     }
 
+    /**Метод возвращает бин-источник данных для целей тестирования*/
+    @Bean("dataSource")
+    @Profile("test")
+    public DriverManagerDataSource getDriverManagerTest()
+    {
+        DriverManagerDataSource driverManager = new DriverManagerDataSource();
+        driverManager.setDriverClassName("org.postgresql.Driver");
+        driverManager.setUrl("jdbc:postgresql://localhost:5432/banners_admin_db_test");
+        driverManager.setUsername("postgres");
+        driverManager.setPassword("rgb111RGB");
+        return driverManager;
+    }
+
     /*JdbcTemplate это базовый класс, который управляет обработкой всех событий и связями с БД.
      Он выполняет SQL-запросы, выполняет итерации по ResultSet и извлекает вызываемые значения,
      обновляет инструкции и вызовы процедур, “ловит” исключения и транслирует их в исключения,
      определённые в пакете org.springframwork.dao .*/
     @Bean("jdbcTemplate")
-    public JdbcTemplate getJdbcTemplate()
+    public JdbcTemplate getJdbcTemplate(DataSource dataSource)
     {
-        return new JdbcTemplate(getDriverManager());
+        return new JdbcTemplate(dataSource);
     }
+
 }
